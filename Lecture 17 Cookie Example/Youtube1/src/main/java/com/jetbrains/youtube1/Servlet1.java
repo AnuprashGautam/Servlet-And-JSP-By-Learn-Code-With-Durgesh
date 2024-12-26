22f54c74a8480ebb155485f7d1e0f1a754436e85
Anuprash Gautam <anuprashgautam@hotmail.com>
2024-12-26 13:01:28 +0530

    State management with Cookie

---
+++ Lecture 17 Cookie Example/Youtube1/src/main/java/com/jetbrains/youtube1/Servlet1.java
@@ -0,0 +1,27 @@
+package com.jetbrains.youtube1;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Servlet1 extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name=request.getParameter("name");
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello "+name+" welcome to my website...</h1>");
+        out.println("<h1><a href='servlet2'>Go to Servlet 2</a></h1>");
+
+
+        // Creating cookie
+
+        Cookie c=new Cookie("user_name",name);
+        response.addCookie(c);
+    }
+}
---
+++ Lecture 17 Cookie Example/Youtube1/src/main/java/com/jetbrains/youtube1/Servlet2.java
@@ -0,0 +1,46 @@
+package com.jetbrains.youtube1;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Servlet2 extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        // Getting cookie
+
+        boolean flag = false;
+        String name="";
+
+        Cookie cookies[] = request.getCookies();
+
+        if(cookies == null) {
+            out.println("<h1> You are the new user. Go to home page and submit your name.");
+            return;
+        }
+        else{
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("user_name")) {
+                    flag = true;
+                    name = cookie.getValue();
+                }
+            }
+        }
+
+        if(flag){
+            out.println("<h1>Hello "+name+" welcome back to my website...</h1>");
+            out.println("<h1>Thank you</h1>");
+        }
+        else{
+            out.println("<h1> You are the new user. Go to home page and submit your name.");
+        }
+    }
+}
